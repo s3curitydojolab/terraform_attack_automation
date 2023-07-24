f39ffa46188c0b649bf7b8700b824c00a65b0d35
s3curitydojolab <128726404+s3curitydojolab@users.noreply.github.com>
2023-07-25 04:46:47 +0530

    Update s3_bucket.tf

--- templates/s3_bucket.tf
+++ templates/s3_bucket.tf
@@ -1,13 +1,13 @@
 # This file is for demonstrating that apply_on_plan.sh is possible by generating an S3 bucket
+provider "aws" {
+  region = "us-east-1"
+}
+
 resource "random_string" "random_name" {
   length    = 8
   min_lower = 8
 }
 
-resource "aws_s3_bucket_acl" "bucket" {
-  bucket = aws_s3_bucket.bucket.id
-  acl    = "private"
-}
 
 resource "aws_s3_bucket" "bucket" {
   bucket = "remove-this-test-bucket-${random_string.random_name.result}"
@@ -16,4 +16,9 @@ resource "aws_s3_bucket" "bucket" {
     Name        = "Remove-This"
     Environment = "Test-Env"
   }
+}
+
+output "bucket_name" {
+  description = "The name of the S3 bucket"
+  value       = aws_s3_bucket.bucket.id
 }
